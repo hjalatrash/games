a7217d04890b087dbac5dfff5a415320cce0e983
Hussam Alatrash <hjalatrash@gmail.com>
2024-01-07 23:27:56 -0800

    Some refactoring

--- SDL2_test/gameloop.h
+++ SDL2_test/gameloop.h
@@ -1,14 +1,10 @@
 #ifndef GAMELOOP_H
     #define GAMELOOP_H
 
-    #define SQUARE_SIZE (12)
     #define X_MAX       (96)
     #define Y_MAX       (64)
     #define MAX_MOVES   (100)
     #define MAX_LEVEL   (5)
-
-    #define SCREEN_WIDTH    (SQUARE_SIZE * X_MAX)
-    #define SCREEN_HEIGHT   (SQUARE_SIZE * Y_MAX)
 
     bool game_step(void);
     void game_init();
--- SDL2_test/rendering.h
+++ SDL2_test/rendering.h
@@ -3,6 +3,11 @@
 
     #include "gameloop.h"
 
+    #define SQUARE_SIZE (12)
+    #define SCREEN_WIDTH    (SQUARE_SIZE * X_MAX)
+    #define SCREEN_HEIGHT   (SQUARE_SIZE * Y_MAX)
+
+
     int setup_graphics_window(void);
     void render(unsigned int squares[X_MAX][Y_MAX]);
     void close_graphics(void);
