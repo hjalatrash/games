3fa6a4e1bacfb8c132275dbe7e3020d1c811fd68
Hussam Alatrash <hjalatrash@gmail.com>
2024-01-07 18:59:45 -0800

    Added text

--- SDL2_test/rendering.cpp
+++ SDL2_test/rendering.cpp
@@ -2,16 +2,26 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL_ttf.h>
 #include <windows.h>
+
 
 SDL_Window *window;
 SDL_Renderer *renderer;
+TTF_Font *font;
 
 int setup_graphics_window(void)
 {
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("SDL_Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Initialize SDL_ttf
+    if (TTF_Init() != 0) {
+        printf("SDL_ttf initialization failed: %s\n", TTF_GetError());
+        SDL_Quit();
         return 1;
     }
 
@@ -33,9 +43,38 @@ int setup_graphics_window(void)
         return 1;
     }
 
+    // Load a font
+    //font = TTF_OpenFont("./font/ArianaVioleta-dz2K.ttf", 24);
+    font = TTF_OpenFont("./font/Debrosee-ALPnL.ttf", 24);
+
+    if (!font) {
+        printf("Failed to load font: %s\n", TTF_GetError());
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        TTF_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
     return 0;
 }
 
+void print_status(void)
+{
+    SDL_Texture *textTexture;
+    SDL_Color textColor = {255, 255, 255, 255}; // White color
+
+    // Create a surface with text
+    SDL_Surface *textSurface = TTF_RenderText_Solid(font, "Hello, SDL!", textColor);
+
+    // Create a texture from the surface
+    textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
+
+    // Render the text
+    SDL_Rect textRect = {100, 100, textSurface->w, textSurface->h};
+    SDL_RenderCopy(renderer, textTexture, NULL, &textRect);
+
+}
 
 void render(unsigned int squares[X_MAX][Y_MAX])
 {
@@ -82,6 +121,8 @@ void render(unsigned int squares[X_MAX][Y_MAX])
             SDL_RenderFillRect(renderer, &rectangle);
         }
     }
+
+    print_status();
 
     // Update the screen
     SDL_RenderPresent(renderer);
